d364816ee797f3902f0192196ad36ddf585ff06b
Omar Jaber <omarpp5555@gmail.com>
2026-03-23 15:22:10 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+using CompanyEmployees.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(),
+    "/nlog.config"));
+
+builder.Services.AddControllers();
+
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerService();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+app.UseCors("CorsPolicy"); 
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
